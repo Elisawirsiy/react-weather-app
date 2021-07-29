d08c68b0393ecba7c6020e2641a4ce3991030dfb
Elizabeth Wirsiy <elisawirsiy@gmail.com>
2021-07-29 03:44:30 +0300

    Update SearchEngine.js

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import WeatherConditions from "./WeatherConditions";
-import WeatherIcon from "./WeatherIcon";
+
 import "./SearchEngine.css";
 
 export default function SearchEngine() {
